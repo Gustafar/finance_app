414a3ba292ae4d99c3cdd201ca33c3e241f6ec4e
Gustafar <gustavomarquesdesouzaoliveira@gmail.com>
2026-08-19 15:43:50 -0300

    Widen description columns to TEXT (was VARCHAR(255))

---
+++ migrations/000008_widen_description_to_text.down.sql
@@ -0,0 +1,3 @@
+ALTER TABLE expenses ALTER COLUMN description TYPE VARCHAR(255);
+ALTER TABLE recurring_expenses ALTER COLUMN description TYPE VARCHAR(255);
+ALTER TABLE installment_purchases ALTER COLUMN description TYPE VARCHAR(255);
---
+++ migrations/000008_widen_description_to_text.up.sql
@@ -0,0 +1,3 @@
+ALTER TABLE expenses ALTER COLUMN description TYPE TEXT;
+ALTER TABLE recurring_expenses ALTER COLUMN description TYPE TEXT;
+ALTER TABLE installment_purchases ALTER COLUMN description TYPE TEXT;
